ebbf686dc502a9919d65abc22ce6ba1af22870d6
shaunak kapur <shaunak1206@gmail.com>
2025-05-30 14:20:46 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = "pk.eyJ1IjoiYmFiYnMiLCJhIjoiY2s1b2JoMjZvMGYydzNmbXAxMXp1NWZhZyJ9.LEHmtAFLAij67eF-54FjxA";
+const map = new mapboxgl.Map({
+   container: "map", // container ID
+   style: "mapbox://styles/babbs/clk2po6u801nc01no7x6c6pj4", // style URL
+   center: [-122.4, 37.78], // starting position [lng, lat]
+   zoom: 9, // starting zoom
+});
